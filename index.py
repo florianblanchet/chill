ca750bc6600103ec1b0e1b7c3f839164e9d8eae5
florianblanchet <florianblanchet@orange.fr>
2017-04-23 01:15:03 +0800

    hkl

--- index.py
+++ index.py
@@ -327,7 +327,7 @@ start_time = time.time()
 
 def run_every_10_seconds():
     print("Running periodic task!")
-    print "Elapsed time: " + str(time.time() - start_time)
+    print("Elapsed time: " + str(time.time() - start_time))
 
 def run_schedule():
     while 1:
