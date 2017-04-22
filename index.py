5d8a01ab3ea8a33cb905dd1096b728f1c54b157f
florianblanchet <florianblanchet@orange.fr>
2017-04-22 22:29:55 +0800

    re

--- index.py
+++ index.py
@@ -9,6 +9,14 @@ import schedule
 from sqlalchemy import *
 SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_PUCE_URL')
 token = os.environ.get('FB_ACCESS_TOKEN')
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+engine.echo = False
+metadata = MetaData(engine)
+users = Table('user', metadata, autoload=True)
+news = Table('news', metadata, autoload=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 app = Flask(__name__)
 
