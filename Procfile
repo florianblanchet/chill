2349ca0eb6e3e1e0d67de3ac8c35419309b98303
florianblanchet <florianblanchet@orange.fr>
2017-04-22 21:16:37 +0800

    re

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 index.py 
+web: gunicorn index:app
--- index.py
+++ index.py
@@ -2,5 +2,6 @@ import requests
 import os
 token = os.environ.get('FB_ACCESS_TOKEN')
 texte ='salut'
-payload = {'recipient': {'id': '1086165011488571'}, 'message': {'text': texte}}
+payload = {'recipient': {'id': '1285839391505940'}, 'message': {'text': texte}}
 r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+
