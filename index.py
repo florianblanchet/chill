c31d136c7227a7090dfed81c55f0714999dea53f
florianblanchet <florianblanchet@orange.fr>
2017-04-22 22:28:11 +0800

    re

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
 from flask import Flask
+import requests
+import os
+import urllib.request
+from bs4 import BeautifulSoup
+from sqlalchemy.orm import sessionmaker
+import time
+import schedule
+from sqlalchemy import *
+SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_PUCE_URL')
+token = os.environ.get('FB_ACCESS_TOKEN')
+
 app = Flask(__name__)
 
 @app.route('/')
